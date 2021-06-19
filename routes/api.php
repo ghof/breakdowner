757864f71cb16910cf0b62fa3e098692d2d21650
ghofrane <gh.hmida@wssel.com>
2021-06-20 00:06:19 +0100

    ::fire: publish docker files and fix unauthenticated issue

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::middleware('auth:api')->group(function () {
+Route::middleware('client_credentials')->group(function () {
     Route::resource('breakdowns', BreakdownController::class)->only(['store', 'index']);
 });
